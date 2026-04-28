c93eef76106086fc7e40ed84a0e3a7f69b3b6e40
jahnavipadmanabhuni <https://github.com/Jahnavipadmanabhuni>
2026-04-28 11:34:50 +0530

    add home controller

---
+++ src/main/java/com/handloom/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.handloom.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Handloom Backend API is running! ✅";
+    }
+}
